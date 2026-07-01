fe221ebce39f9158f3680975e7787671648d3394
gutao.123 <gutao.123@bytedance.com>
2026-07-01 18:06:43 +0800

    线程

--- 1.py/14.线程与进程/2.多线程.py
+++ 1.py/14.线程与进程/2.多线程.py
@@ -55,3 +55,26 @@ if __name__ == "__main__":
 # 线程1 11111
 # 线程2 00000
 # ........
+
+
+## 2.自定义Thread子类创建线程
+import time
+import threading
+
+class Worker(threading.Thread):
+  def __init__(self, name):
+    super().__init__()
+    self.name = name
+
+  def run(self):
+    flag = 0
+    while True:
+      print(f"\r{self.name}:{str(flag)*5}", end="")
+      flag = flag ^ 1 # 替换0和1
+      time.sleep(0.2)
+
+if __name__ == "__main__":
+  t1 = Worker("线程1")
+  t2 = Worker("线程2")
+  t1.start()
+  t2.start()
